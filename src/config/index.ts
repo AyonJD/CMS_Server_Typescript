01db36c869d8329d1aa425113c7e6b69270f6f6e
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-05-30 16:33:17 +0600

    [Done] Server setup

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({
+  path: path.join(process.cwd(), '.env'),
+})
+
+export default {
+  port: process.env.PORT || 8000,
+  database_string: process.env.DATABASE_STRING,
+  access_token: process.env.ACCESS_TOKEN,
+  app_email: process.env.APP_EMAIL,
+  app_password: process.env.APP_PASSWORD,
+}
