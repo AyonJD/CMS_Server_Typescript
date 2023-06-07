6cd1f93c4327f4ebdcbf15d349fc479d1c06ef71
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-07 13:32:02 +0600

    [Done] validation error handler

---
+++ src/errors/handleValidationError.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+import {
+  IGenericErrorResponse,
+  IGenericErrorMessage,
+} from '../interfaces/errorInterface'
+
+const handleValidationError = (
+  error: mongoose.Error.ValidationError
+): IGenericErrorResponse => {
+  const errorMessages: IGenericErrorMessage[] = Object.values(error.errors).map(
+    (el: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
+      return {
+        message: el.message,
+        path: el.path,
+      }
+    }
+  )
+
+  const statusCode = 400
+  return {
+    status: 'false',
+    statusCode,
+    message: 'Validation Error',
+    errorMessages,
+  }
+}
+
+export default handleValidationError
